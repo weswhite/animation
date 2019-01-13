9a6f80884c460cb1b613a33974e01ffd53cce662
Wes White <wtealwhite@gmail.com>
2019-01-13 17:07:00 -0600

    added routes to show other animations

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
 import './App.css';
 import Fireworks from './fireworks/fireworks';
 import Sphere from './sphere/sphere';
@@ -7,9 +9,27 @@ import FollowPath from './follow-path/follow-path';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Fireworks></Fireworks>
-      </div>
+      <Router>
+        <div className="App">          
+          <header>
+            <Link to="/fireworks">
+              <button>Fireworks</button>
+            </Link>
+            <Link to="/sphere">
+              <button>Sphere</button>
+            </Link>  
+            <Link to="/follow-path">
+              <button>Follow Path</button>
+            </Link>
+          </header>
+          <div>
+            <Route exact path="/" component={Fireworks} />
+            <Route exact path="/fireworks" component={Fireworks} />
+            <Route exact path="/sphere" component={Sphere} />
+            <Route exact path="/follow-path" component={FollowPath} />
+          </div>
+        </div>
+      </Router>
     );
   }
 }
